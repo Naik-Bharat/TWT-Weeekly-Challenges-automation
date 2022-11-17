7f9dfefbd50ce442edd3b607ea81cdae62fe7712
Bharat Naik <bharatnaik777@gmail.com>
2022-11-17 19:53:38 +0530

    Added functionality to get git files from a git directory and some refactoring

--- automation.go
+++ automation.go
@@ -9,37 +9,51 @@ import (
 	"strings"
 )
 
+type git_page struct {
+	link string
+}
+
+// this function returns a slice of files in a git page
+func (page git_page) get_git_files() []string {
+	// list of all files
+	files := []string{}
+
+	// content in the page as a slice
+	body_array := get_url(page.link)
+	for index, line := range body_array {
+		// if it is a file
+		if strings.Contains(line, "rowheader") {
+			words_array := strings.Split(body_array[index+1], " ")
+			for _, word := range words_array {
+				// if this word contains the title
+				if strings.Contains(word, "title") {
+					file := word[len("title")+2 : len(word)-1]
+					files = append(files, file)
+				}
+			}
+		}
+	}
+
+	return files
+}
+
 func main() {
 	challenge_number := get_challenge_number()
 	println(challenge_number)
-	wget("https://raw.githubusercontent.com/Naik-Bharat/TWT-Weekly-Challenges/master/Challenge_100/Solution.py", "test.py")
 }
 
 // This function finds the latest challenge number by scraping the tester's github page
 func get_challenge_number() int {
-	const URL = "https://github.com/Pomroka/TWT_Challenges_Tester/"
-	resp, err := http.Get(URL)
-	if err != nil {
-		println("Error downloading tester's webpage!")
-		panic(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		println("Error reading content from tester's webpage!")
-		panic(err)
-	}
+	var challenge_number int
 
-	// looking for the latest challenge number from the page
-	body_array := strings.Split(string(body), "\n")
-	challenge_number := 0
-
-	for _, line := range body_array {
-		if strings.Contains(line, "Challenge_") {
-			index := strings.Index(line, "Challenge_")
-			curr_challenge_number, err := strconv.Atoi(line[index+len("Challenge_") : index+len("Challenge_")+3])
+	page := git_page{"https://github.com/Pomroka/TWT_Challenges_Tester/"}
+	files := page.get_git_files()
+	for _, file := range files {
+		if strings.Contains(file, "Challenge_") {
+			curr_challenge_number, err := strconv.Atoi(file[len("Challenge_"):])
 			if err != nil {
-				continue
+				println("Error converting challenge number to integer")
+				panic(err)
 			}
 			if challenge_number < curr_challenge_number {
 				challenge_number = curr_challenge_number
@@ -47,19 +61,34 @@ func get_challenge_number() int {
 		}
 	}
 
-	println("Challenge Number: {}", challenge_number)
 	return challenge_number
 }
 
+func get_url(link string) []string {
+	resp, err := http.Get(link)
+	if err != nil {
+		println("Error downloading", link)
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		println("Error reading content from", link)
+		panic(err)
+	}
+	return strings.Split(string(body), "\n")
+}
+
+// function to wget files
 func wget(link string, file_path string) {
 	cmd := exec.Command("wget", link, "-O", file_path)
-	stdout, err := cmd.Output()
+	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
 	}
-	println(string(stdout))
 }
 
+// function to handle moving directories
 func move_directory(old_path string, new_path string) {
 	err := os.Rename(old_path, new_path)
 	if err != nil {
