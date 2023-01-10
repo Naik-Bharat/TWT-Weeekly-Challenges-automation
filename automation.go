981f625375ee127b680255a56b398b34b8b2aa8e
Bharat Naik <bharatnaik777@gmail.com>
2023-01-11 00:21:53 +0530

    switched to camelCase

--- automation.go
+++ automation.go
@@ -10,22 +10,22 @@ import (
 	"strings"
 )
 
-type git_page struct {
+type gitPage struct {
 	link string
 }
 
 // this function returns a slice of files in a git page
-func (page git_page) get_git_files() []string {
+func (page gitPage) getGitFiles() []string {
 	// list of all files
 	files := []string{}
 
 	// content in the page as a slice
-	body_array := get_url(page.link)
-	for index, line := range body_array {
+	bodyArray := getUrl(page.link)
+	for index, line := range bodyArray {
 		// if it is a file
 		if strings.Contains(line, "rowheader") {
-			words_array := strings.Split(body_array[index+1], " ")
-			for _, word := range words_array {
+			wordsArray := strings.Split(bodyArray[index+1], " ")
+			for _, word := range wordsArray {
 				// if this word contains the title
 				if strings.Contains(word, "title") {
 					file := word[len("title")+2 : len(word)-1]
@@ -39,46 +39,46 @@ func (page git_page) get_git_files() []string {
 }
 
 func main() {
-	challenge_number := get_challenge_number()
-	folder_restructure(challenge_number)
-	download_files(challenge_number)
+	challengeNumber := getChallengeNumber()
+	folderRestructure(challengeNumber)
+	downloadFiles(challengeNumber)
 }
 
-func download_files(challenge_number int) {
-	page := git_page{fmt.Sprintf("https://github.com/Pomroka/TWT_Challenges_Tester/tree/main/Challenge_%v", challenge_number)}
-	files := page.get_git_files()
+func downloadFiles(challengeNumber int) {
+	page := gitPage{fmt.Sprintf("https://github.com/Pomroka/TWT_Challenges_Tester/tree/main/Challenge_%v", challengeNumber)}
+	files := page.getGitFiles()
 	for _, file := range files {
 		if file != "G" {
-			wget(fmt.Sprintf("https://raw.githubusercontent.com/Pomroka/TWT_Challenges_Tester/main/Challenge_%v/%v", challenge_number, file), fmt.Sprintf("Challenge_%v/%v", challenge_number, file))
+			wget(fmt.Sprintf("https://raw.githubusercontent.com/Pomroka/TWT_Challenges_Tester/main/Challenge_%v/%v", challengeNumber, file), fmt.Sprintf("Challenge_%v/%v", challengeNumber, file))
 		}
 	}
 }
 
 // This function finds the latest challenge number by scraping the tester's github page
-func get_challenge_number() int {
-	var challenge_number int
+func getChallengeNumber() int {
+	var challengeNumber int
 
-	page := git_page{"https://github.com/Pomroka/TWT_Challenges_Tester/"}
-	files := page.get_git_files()
+	page := gitPage{"https://github.com/Pomroka/TWT_Challenges_Tester/"}
+	files := page.getGitFiles()
 	for _, file := range files {
 		if strings.Contains(file, "Challenge_") {
-			curr_challenge_number, err := strconv.Atoi(file[len("Challenge_"):])
+			currChallengeNumber, err := strconv.Atoi(file[len("Challenge_"):])
 			if err != nil {
 				println("Error converting challenge number to integer")
 				panic(err)
 			}
-			if challenge_number < curr_challenge_number {
-				challenge_number = curr_challenge_number
+			if challengeNumber < currChallengeNumber {
+				challengeNumber = currChallengeNumber
 			}
 		}
 	}
 
-	fmt.Println("Challenge Number:", challenge_number)
-	return challenge_number
+	fmt.Println("Challenge Number:", challengeNumber)
+	return challengeNumber
 }
 
 // function to read a url and return a slice of its content
-func get_url(link string) []string {
+func getUrl(link string) []string {
 	resp, err := http.Get(link)
 	if err != nil {
 		println("Error downloading", link)
@@ -94,8 +94,8 @@ func get_url(link string) []string {
 }
 
 // function to wget files
-func wget(link string, file_path string) {
-	_, err := os.OpenFile(file_path, os.O_RDONLY, 0750)
+func wget(link string, filePath string) {
+	_, err := os.OpenFile(filePath, os.O_RDONLY, 0750)
 	if err == nil {
 		var choice string
 		fmt.Printf("%v already exists. Do you want to rewrite? (y/N) ", link)
@@ -104,7 +104,7 @@ func wget(link string, file_path string) {
 			return
 		}
 	}
-	cmd := exec.Command("wget", link, "-O", file_path)
+	cmd := exec.Command("wget", link, "-O", filePath)
 	_, err = cmd.Output()
 	if err != nil {
 		panic(err)
@@ -113,20 +113,20 @@ func wget(link string, file_path string) {
 }
 
 // this function returns whether a folder should be moved to previous challenges folder
-func old_challenge(file string, challenge_number int) bool {
-	file_challenge_number, err := strconv.Atoi(file[len("Challenge_"):])
+func oldChallenge(file string, challengeNumber int) bool {
+	fileChallengeNumber, err := strconv.Atoi(file[len("Challenge_"):])
 	if err != nil {
 		println("Cannot convert challenge number to int in challenge tester's git repo")
 		panic(err)
 	}
 
-	challenge_diff := challenge_number - file_challenge_number
-	return challenge_diff > 2
+	challengeDiff := challengeNumber - fileChallengeNumber
+	return challengeDiff > 2
 }
 
 // function to handle folder re-structuring
-func folder_restructure(challenge_number int) {
-	new_directory(fmt.Sprintf("Challenge_%v", challenge_number))
+func folderRestructure(challengeNumber int) {
+	newDirectory(fmt.Sprintf("Challenge_%v", challengeNumber))
 	files, err := os.ReadDir("./")
 	if err != nil {
 		println("Cannot read current directory")
@@ -134,9 +134,9 @@ func folder_restructure(challenge_number int) {
 	}
 	for _, file := range files {
 		if strings.Contains(file.Name(), "Challenge_") {
-			if old_challenge(file.Name(), challenge_number) {
-				new_directory("Previous_Challenges")
-				move_directory(file.Name(), fmt.Sprintf("Previous_Challenges/%v", file.Name()))
+			if oldChallenge(file.Name(), challengeNumber) {
+				newDirectory("Previous_Challenges")
+				moveDirectory(file.Name(), fmt.Sprintf("Previous_Challenges/%v", file.Name()))
 				fmt.Printf("Moved %v to %v\n", file.Name(), fmt.Sprintf("Previous_Challenges/%v", file.Name()))
 			}
 		}
@@ -144,16 +144,16 @@ func folder_restructure(challenge_number int) {
 }
 
 // function to handle moving directories
-func move_directory(old_path string, new_path string) {
-	err := os.Rename(old_path, new_path)
+func moveDirectory(oldPath string, newPath string) {
+	err := os.Rename(oldPath, newPath)
 	if err != nil {
-		println("Error moving ", old_path)
+		println("Error moving ", oldPath)
 		panic(err)
 	}
 }
 
 // function to create new directories
-func new_directory(path string) {
+func newDirectory(path string) {
 	err := os.Mkdir(path, 0750)
 	if err == nil {
 		fmt.Printf("New Directory %v created\n", path)
